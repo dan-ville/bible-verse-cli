7878d2aa6bf057059b1e8d729f79fe3ea9785dae
Daniel Villegas <daniel.villegas@fsgi.com>
2023-12-04 20:16:51 -0500

    fix var bug, add flags

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from services.esv_api_service import ESVService
 
 def main():
     parser = argparse.ArgumentParser(description="Retrieve Bible verses from the ESV API.")
-    parser.add_argument('reference', type=str, help="Bible verse reference (e.g., John 11:35)", default=None)
+    parser.add_argument('-r', '--reference', type=str, help="Bible verse reference (e.g., John 11:35)", default=None)
 
     args = parser.parse_args()
 
@@ -12,7 +12,7 @@ def main():
     
     if args.reference:
         # Get the verse via the reference
-        verse = esv_service.get_bible_verse(args.reference, args.mode)
+        verse = esv_service.get_bible_verse(args.reference)
         print(verse)
     else:
         # Handle the case where no reference is provided
